88c898a8e3930a114f3af531029c78efe9987822
Smolu <smolunpapu@gmail.com>
2023-05-07 02:04:51 +0300

    blackjack working

--- BlackJack.py
+++ BlackJack.py
@@ -1,75 +1,126 @@
 import random
+import sys
 
 
 def main():
-
+    global deck
     deck = []
-    for i in range(1, 4):
-        for j in range(1, 15):
-            deck.append(j)
-
-    random.shuffle(deck)  # shuffle deck
-    money = 500
+    money = 1000
+    global player
+    global dealer
 
     while True:  # game loop start
-        print(deck)
-
+        newdeck()
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~")
         print("Money: ", money, "$")
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~")
         if money <= 0:
-            print("No money. GG!")
-            quit()
+            sys.exit("No money. GG!")
+        while True:
+            bet = getBet(money)
+            print("your Bet:", bet, "$")
+            player = []
+            player.append(getcard(deck))
+            player.append(getcard(deck))
+            dealer = []
+            dealer.append(getcard(deck))
+            printplayers(player, dealer)
+            while True:
+                hands = checkplayers(player, dealer)
+                if hands[0] < 21 and hands[1] <= 16:
+                    getmove()
+                else:
+                    break
 
-        bet = getBet(money)
-        print("your Bet:", bet)
-        hand = []
-        hand.append(getcard(deck))
-        hand.append(getcard(deck))
-        dealer = []
-        dealer.append(getcard(deck))
-        printhands(hand, dealer)
-        move = getmove()
-
-        if move == "H":  # HIT
-            hand.append(getcard(deck))
-        dealer.append(getcard(deck))
-        printhands(hand, dealer)
-        checkhands(hand, dealer)
-        move = getmove()
-        return  # end
+            if checkwincondition() == "win":
+                money += bet
+                print("YOU WIN!")
+                break
+            elif checkwincondition() == "lose":
+                money -= bet
+                print("YOU LOSE!")
+                break
+            elif checkwincondition() == "push":
+                print("PUSH!")
+                break
 
 
-def checkhands(hand, dealer):
-    handsum = 0
+def newdeck():
+    for i in range(1, 5):
+        for j in range(1, 15):
+            deck.append(j)
+    random.shuffle(deck)
+
+
+def checkplayers(player, dealer):
+    player.sort()
+    dealer.sort()
+    playersum = 0
     dealersum = 0
-    for i in range(len(hand)):
-        if hand[i] == 14:
-            if handsum + 10 > 21:
-                handsum + 1
+    for i in range(len(player)):
+        if player[i] > 10 and player[i] < 14:
+            playersum += 10
+            continue
+        if player[i] == 14:
+            if playersum + 11 > 21:
+                playersum + 1
                 continue
-        handsum += hand[i]
+            else:
+                playersum += 11
+                continue
+        if player[i] <= 10:
+            playersum += player[i]
     for i in range(len(dealer)):
+        if dealer[i] > 10 and dealer[i] < 14:
+            dealersum += 10
+            continue
         if dealer[i] == 14:
-            if dealersum + 10 > 21:
+            if dealersum + 11 > 21:
                 dealersum + 1
                 continue
-        dealersum += dealer[i]
+            else:
+                dealersum += 11
+                continue
+        if dealer[i] <= 10:
+            dealersum += dealer[i]
+
+    return [playersum, dealersum]
 
 
-def printhands(hand, dealer):
-    hand.sort()
+def checkwincondition():
+    result = checkplayers(player, dealer)
+    localplayer = result[0]
+    localdealer = result[1]
+    if (localplayer == 21 and localdealer != 21 or
+            localplayer > localdealer and localplayer <= 21 and localdealer < 21 or
+            localdealer > 21 and localplayer <= 21):
+        return "win"
+    elif localdealer == localplayer:
+        return "push"
+    return "lose"
+
+
+def printplayers(player, dealer):
+    result = checkplayers(player, dealer)
+    player.sort()
     dealer.sort()
-    print("Your Hand: ")
-    for i in range(len(hand)):
-        if hand[i] == 14:
-            print("ACE")
+    print("Player:", end=" ")
+    for i in range(len(player)):
+        if player[i] == 14:
+            print("ACE", end=" ")
             continue
-        print(hand[i])
-    print("Dealers Hand: ")
+        print(player[i], end=" ")
+    print()
+    print("Dealer:", end=" ")
     for i in range(len(dealer)):
         if dealer[i] == 14:
-            print("ACE")
+            print("ACE", end=" ")
             continue
-        print(dealer[i])
+        print(dealer[i], end=" ")
+    print()
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("Player:", result[0], "Dealer:", result[1])
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
 
 def getcard(deck):
@@ -80,14 +131,23 @@ def getcard(deck):
 
 def getmove():
     while True:
-        print("HIT (H) , Stand (S) , Bust (B)")
+        hands = checkplayers(player, dealer)
+        print("HIT (H) , Stand (S)")
         move = input().upper()
         if move == "H":
-            return move
+            player.append(getcard(deck))
+            # check if to draw for dealer
+            if hands[1] < 17:
+                dealer.append(getcard(deck))
+            printplayers(player, dealer)
+            return "H"
         if move == "S":
-            return move
-        if move == "B":
-            return move
+            # while dealer below 16 draw
+            while hands[1] < 17:
+                dealer.append(getcard(deck))
+                printplayers(player, dealer)
+                hands = checkplayers(player, dealer)
+            return "S"
 
 
 def getBet(maxbet):
@@ -95,10 +155,14 @@ def getBet(maxbet):
         print(" Enter Bet: 1 -", maxbet)
         bet = input("> ").upper().strip()
         if not bet.isdecimal():
+            print("Invalid Bet!")
             continue
         bet = int(bet)
-        if bet <= maxbet:
+        if bet <= maxbet and bet > 0:
+            print("~~~~~~~~~~~~~~~~~~~~~~~~~~")
             return bet
+        else:
+            print("Invalid Bet!")
 
 
 main()
