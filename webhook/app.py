2e90217e6caadd673668faf60e92a6819cea88eb
smolu <smolunpapu@gmail.com>
2023-11-09 16:47:45 +0200

    webhook

--- webhook/app.py
+++ webhook/app.py
@@ -1,22 +1,24 @@
 from flask  import Flask,request
-import json
 import os
+from pathlib import Path
 
 app = Flask(__name__)
-filecount = 0
-abs_path = os.getcwd()
-data_path = os.path.join(abs_path,"data/")
-print(data_path)
+path = Path(__file__).parent.resolve()
+print(path)
+
 @app.route("/webhook", methods=["POST"])
 def webhook():
   if request.method == "POST":
     print(f"Data received")
-    data = json.dumps(request.json, indent=4)
-    filename = "data" + str(filecount) + ".json"
-    with open(os.path.join(data_path,filename),"w") as file:
-      file.write(data)
-    print("data saved!")
-    filecount + 1
+    event = request.json["hook"]["events"]
+    repository = request.json["repository"]["name"]
+    if event[0] == "push":
+      Redeploy(repository)
+
+
     return "received"
   
-app.run(host="0.0.0.0",port=8000, debug=True)
+def Redeploy(repository):
+  match repository:
+    case "smolander.dev":
+      os.system("sh Deploy_smolander.dev.sh")
